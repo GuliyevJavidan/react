96c78789961cd3a714a5f0bbe6231f9f98bed0bd
ca r213.17 <ca.r213.17@ca.local>
2023-01-23 16:22:43 +0400

    Hi there

---
+++ src/companents/Navbar/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./navbar.css"
+function Navbar() {
+  return (
+    <>
+    <div className='navbar'>
+        <div className='Notary'>Notary</div>
+        <div><a>Home</a><a>Add</a></div>
+    </div>
+    </>
+  )
+}
+
+export default Navbar
